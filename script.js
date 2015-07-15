d8e224eeff2f3e4f7414dfb59c25cced9fda23d0
arturssmirnovs <arturssmirnovs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 19:43:38 +0000

    delete script
    git-svn-id: https://plugins.svn.wordpress.org/facebook-like-box-pop-up/trunk@1199535 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- script.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-
-	load_fblb();
-	$('#fb_box_close, body').click( function() { unload_fblb(); });
-	function unload_fblb() { $('#fb_box, .fb_overlay').fadeOut("slow"); }
-	function load_fblb() { $('#fb_box, .fb_overlay').fadeIn("slow"); }
-	
-});
